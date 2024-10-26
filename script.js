16c355930d5d4a7118748fe53b1679c0bc1c2a44
Raphael Pedro <116127157+Raphaelmsp@users.noreply.github.com>
2024-10-26 10:27:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -175,7 +175,7 @@ if(index !== -1)
   function checkRestaurantOpen(){
     const data = new Date();
     const hora = data.getHours();
-    return hora >=6 && hora <33; //true
+    return hora >=8 && hora <20; //true
   }
 
   const spanItem = document.getElementById("date-span")
